98e9518f4557e97e93a66d9b9064d1e037d19655
frannunezrivera <frannunezrivera@gmail.com>
2017-02-27 20:43:07 +0900

    Add reducers for getting page detail

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,6 @@
-export const REQUEST_PAGES = 'REQUEST_PAGES'
-export const RECEIVE_PAGES = 'RECEIVE_PAGES'
+import {
+    REQUEST_PAGES, RECEIVE_PAGES, RECEIVE_PAGE, REQUEST_PAGE
+} from '../actions'
 
 const initialState = {
     items: {
@@ -18,6 +19,22 @@ function rootReducer(state = initialState, action) {
         case REQUEST_PAGES:
         case RECEIVE_PAGES:
             return Object.assign({}, state, pages(state, action));
+        case REQUEST_PAGE:
+        case RECEIVE_PAGE:
+            return Object.assign({}, state, pageDetail(state, action));
+        default:
+            return state
+    }
+}
+
+const pageDetail = (state = {}, action) => {
+    switch (action.type) {
+        case RECEIVE_PAGE:
+            return Object.assign({}, state, {
+                pageDetails: {
+                    [action.pageId]: action.detail[action.pageId]
+                },
+            })
         default:
             return state
     }
@@ -67,6 +84,8 @@ export default rootReducer;
 export const getPage = (state, id) =>
     state.pagesById[id];
 
-export const getPages = state => {
-    return state.pageIds.map(id => getPage(state, id));
-}
+export const getPages = state =>
+    state.pageIds.map(id => getPage(state, id));
+
+export const getPageDetail = (state, id) =>
+    state.pageDetails[id];
