9ce9c7b411f2ef93d83beb5d5b608981d368d4dd
frannunezrivera <frannunezrivera@gmail.com>
2017-02-27 20:25:50 +0900

    Add Router

--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ import {Provider} from 'react-redux'
 import thunk from 'redux-thunk'
 import createLogger from 'redux-logger'
 import {createStore, applyMiddleware, combineReducers} from 'redux'
+import {Router, Route, browserHistory} from 'react-router'
+import {syncHistoryWithStore, routerReducer} from 'react-router-redux'
 import {fetchPages} from './actions'
 import rootReducer from './reducers'
 const middleware = [thunk];
@@ -17,16 +19,24 @@ if (process.env.NODE_ENV !== 'production') {
 
 const store = createStore(
     combineReducers({
-        rootReducer
+        rootReducer,
+        routing: routerReducer
     }),
     applyMiddleware(...middleware)
 );
 
 store.dispatch(fetchPages());
 
+const history = syncHistoryWithStore(browserHistory, store);
+
 ReactDOM.render(
     <Provider store={store}>
-        <App />
+        <Router history={history}>
+            <Route path="/" name="root" component={App} title="Pages">
+                <Route path='/page/:id' name="pageDetail" title="Page Detail"/>
+                <Route path='/bookmarks' name="bookmarks" title="Bookmarks"/>
+            </Route>
+        </Router>
     </Provider>,
     document.getElementById('root')
 );
