9576aa03b7b09bc9cd7407fc3681ad408ae4a0bf
frannunezrivera <frannunezrivera@gmail.com>
2017-02-27 21:05:47 +0900

    Update detail container

--- src/containers/DetailContainer/DetailContainer.js
+++ src/containers/DetailContainer/DetailContainer.js
@@ -3,9 +3,13 @@ import {connect} from 'react-redux'
 import {getPageDetail} from '../../reducers/index'
 import PageDetail from '../../components/PageDetail/PageDetail'
 
-import {fetchPage} from '../../actions'
+import {fetchPage, addBookmark, removeBookmark} from '../../actions'
 
 class DetailContainer extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleBookmarkButton = this.handleBookmarkButton.bind(this);
+    }
 
     componentDidMount() {
         const {actions, params, isFetching} = this.props;
@@ -14,8 +18,18 @@ class DetailContainer extends React.Component {
         }
     }
 
+    handleBookmarkButton() {
+        const {pageDetail, isBookmarked, actions} = this.props;
+
+        if (isBookmarked) {
+            return () => actions.removeBookmark(pageDetail.pageid)
+        } else {
+            return () => actions.addBookmark(pageDetail.pageid)
+        }
+    }
+
     render() {
-        const {pageDetail, isFetching} = this.props;
+        const {pageDetail, isFetching, isBookmarked} = this.props;
 
 
         return (<section>
@@ -23,6 +37,8 @@ class DetailContainer extends React.Component {
                 <PageDetail
                     key={pageDetail.pageid}
                     item={pageDetail}
+                    isBookmarked={isBookmarked}
+                    handleBookmarkButton={this.handleBookmarkButton()}
                 />
             }
         </section>)
@@ -31,16 +47,19 @@ class DetailContainer extends React.Component {
 
 function mapStateToProps(state, props) {
     const {id} = props.params;
+    const {bookmarks} = state.rootReducer;
     let isFetching;
 
     const pageDetail = getPageDetail(state.rootReducer, id);
+    const isBookmarked = bookmarks.pageIds.indexOf(parseInt(id, 10)) !== -1;
 
 
     isFetching = !pageDetail;
 
     return {
         pageDetail,
-        isFetching
+        isFetching,
+        isBookmarked
     }
 }
 
@@ -49,6 +68,8 @@ function mapDispatchToProps(dispatch) {
     return {
         actions: {
             fetchPage: (pageId) => dispatch(fetchPage(pageId)),
+            addBookmark: (pageId) => dispatch(addBookmark(pageId)),
+            removeBookmark: (pageId) => dispatch(removeBookmark(pageId)),
         }
     }
 }
