5213cbc567491b72e7fa5097fa692cff6107516f
frannunezrivera <frannunezrivera@gmail.com>
2017-02-27 21:10:28 +0900

    Add Header component

---
+++ src/components/Header/Header.js
@@ -0,0 +1,31 @@
+import React, {Component} from 'react';
+import {Link} from 'react-router'
+
+class Header extends Component {
+    render() {
+        const links = [
+            {to: '/', text: 'Pages'},
+            {to: '/bookmarks', text: 'Bookmarks'}
+        ];
+
+        const {title, pathName} = this.props;
+        return <header>
+            <h1>{title}</h1>
+            <nav>
+                {
+                    links.map((link, i) => {
+                        let linkClass = (pathName === link.to) ?
+                            'link disabled-link' : 'link active-link';
+                        return (
+                            <Link key={i} className={linkClass} to={link.to}>
+                                <span className='link-text'>{link.text}</span>
+                            </Link>
+                        )
+                    })
+                }
+            </nav>
+        </header>;
+    }
+}
+
+export default Header;
