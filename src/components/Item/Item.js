e7a6614f13f2b00b3b53a6b909d01a71a8cc3706
frannunezrivera <frannunezrivera@gmail.com>
2017-02-27 20:43:43 +0900

    Update item component - Add link to page detail

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -1,9 +1,12 @@
 import React, {Component} from 'react';
+import {Link} from 'react-router'
 
 class Item extends Component {
     render() {
-        const {title} = this.props.item;
-        return <li><span>{title}</span></li>;
+        const {title, pageid} = this.props.item;
+        return <li><Link to={"/page/" + pageid}>
+            <span>{title}</span>
+        </Link></li>;
     }
 }
 
