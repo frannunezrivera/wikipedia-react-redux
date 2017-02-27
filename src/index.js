8851d471fbbde6c7825249feb74c594be0164062
frannunezrivera <frannunezrivera@gmail.com>
2017-02-27 20:04:29 +0900

    Set up app index for working with redux
    -Added redux-logger
    -Also dispatch first action

--- src/index.js
+++ src/index.js
@@ -1,9 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './components/App/App';
 import './index.scss';
 
+import {Provider} from 'react-redux'
+import thunk from 'redux-thunk'
+import createLogger from 'redux-logger'
+import {createStore, applyMiddleware, combineReducers} from 'redux'
+import {fetchPages} from './actions'
+import rootReducer from './reducers'
+const middleware = [thunk];
+
+if (process.env.NODE_ENV !== 'production') {
+    middleware.push(createLogger());
+}
+
+const store = createStore(
+    combineReducers({
+        rootReducer
+    }),
+    applyMiddleware(...middleware)
+);
+
+store.dispatch(fetchPages());
+
 ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
 );
